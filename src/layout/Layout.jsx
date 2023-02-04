98a6e75ccae4bbd8c428f77d9cf6c2de0112884d
David Ukelere <david.ukelere291@gmail.com>
2023-02-04 06:38:12 +0100

    Added auth pages

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,10 +4,10 @@ import Header from './../components/Header';
 
 const Layout = () => {
     return (
-        <>
+        <div className="p-4 flex flex-col min-h-screen">
             <Header />
             <Outlet />
-        </>
+        </div>
     )
 }
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const LoginPage = () => {
+const RegisterPage = () => {
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="-mt-32">
-                <h1 className="text-4xl text-center mb-4">Login</h1>
+                <h1 className="text-4xl text-center mb-4">Register</h1>
                 <form className="max-w-lg mx-auto" action="">
+                    <input type="text" placeholder="John Doe" />
                     <input type="email" placeholder="your@email.com" />
                     <input type="password" placeholder="password" />
-                    <button className="primary">Login</button>
+                    <button className="primary">Register</button>
                     <div className="text-center py-2 text-gray-500">
-                        Don't have an account?
-                        <Link className="underline text" to={"/register"}>Register Now</Link>
+                        Have an account?
+                        <Link className="underline text" to={"/login"}>Login</Link>
                     </div>
                 </form>
             </div>
@@ -20,4 +21,4 @@ const LoginPage = () => {
     )
 }
 
-export default LoginPage
+export default RegisterPage;
