8ef85a25bfdeebb744ca7962e7bd8582b774f177
David Ukelere <david.ukelere291@gmail.com>
2023-02-11 17:06:38 +0100

    Added register routes

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const registerUser = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('/register', {
+                name, email, password
+            });
+            alert('Registration successful')
+        } catch (err) {
+            alert('Registration failed')
+        }
+    }
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="-mt-32">
                 <h1 className="text-4xl text-center mb-4">Register</h1>
-                <form className="max-w-lg mx-auto" action="">
-                    <input type="text" placeholder="John Doe" />
-                    <input type="email" placeholder="your@email.com" />
-                    <input type="password" placeholder="password" />
+                <form className="max-w-lg mx-auto" onSubmit={registerUser}>
+                    <input type="text"
+                        placeholder="John Doe"
+                        value={name}
+                        onChange={event => setName(event.target.value)} />
+                    <input type="email"
+                        placeholder="your@email.com"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)} />
+                    <input type="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)} />
                     <button className="primary">Register</button>
                     <div className="text-center py-2 text-gray-500">
                         Have an account?
